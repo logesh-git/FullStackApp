f72dab59ec0af36e002e4f923bb2ce2e917b286d
Logesh J <Logesh.J@gds.ey.com>
2020-02-24 17:24:06 +0530

    firstCommit

---
+++ MyFirstFullStackApp/src/main/java/com/logesh/app/MyFirstFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package com.logesh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstFullStackAppApplication.class, args);
+	}
+
+}
